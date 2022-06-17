6d962edf0df0c444a446945bdc505ff25a68749b
19Moriya <so16.forest.31@gmail.com>
2022-06-17 23:41:15 +0900

    first commit

---
+++ insert.php
@@ -0,0 +1,32 @@
+<?php
+
+$name = $_POST['name'];
+$url = $_POST['url'];
+$comment = $_POST['comment'];
+
+//2. DB接続します
+try {
+  //Password:MAMP='root',XAMPP=''
+  $pdo = new PDO('mysql:dbname=moriya_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {exit('DBConnection Error:'.$e->getMessage());};
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("insert into gs_bm_table(name, url, comment, date) values(:name, :url, :comment, sysdate())");
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("SQL_ERROR:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: index.php");
+  exit();
+}
+?>
+
+
